874bf41bf9427b1c1e7b600c692c62ea84c5b10e
anjalichourasia <anjalichourasia1998@gmail.com>
2024-11-01 13:21:22 +0530

    Split code into a file

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {}, "Namaste EveryOne!");
+    const heading1 = React.createElement("h1", 
+    {
+        id: "title"
+    }, "Namaste EveryOne heading!");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    const footer = ReactDOM.createRoot(document.getElementById("footer"));
+    root.render(heading);
+    footer.render(heading1);
