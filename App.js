820b0f92214f75719395efb47ffabadf4fa275a0
anjalichourasia <anjalichourasia1998@gmail.com>
2024-11-03 00:30:01 +0530

    Write in jsx

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Namaste EveryOne!");
-    const heading1 = React.createElement("h1", 
-    {
-        id: "title"
-    }, "Namaste EveryOne heading!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    const footer = ReactDOM.createRoot(document.getElementById("footer"));
-    root.render(heading);
-    footer.render(heading1);
+// JSX - javascript syntax to create react element
+const jsxElement = <h1>namaste react using jsx</h1>
+
+// React.createElement => Object => HTML Element (render inside a dom it become html)
+const heading = React.createElement("h1", {}, "namaste");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxElement);
