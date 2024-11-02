8e78013e5f105517fc68d77efe24c97f13730b7b
anjalichourasia <anjalichourasia1998@gmail.com>
2024-11-03 01:00:31 +0530

    render functional component

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React functional component 
+//js function - it return jsx code or react element
+const HeaderComponent = () => {
+    return(
+        <div>
+            <div className="nav-items">
+                 <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
 
+                 </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return(
+        {
+        /**
+         * Header
+         *  - logo
+         *  - list Items
+         * body
+         *  - Search bar
+         *  - Restraunt List
+         *      - Restaunat Card
+         *          - Image
+         *          - Name
+         *          - rating
+         *          - Cusins
+         *  footer
+         *   - links
+         *   - copyright
+         */
+        }
+    )
+}
 // JSX - javascript syntax to create react element
-const jsxElement = <h1>namaste react using jsx</h1>
+//React element
+const jsxElement = (
+    <h1>
+        namaste react using jsx
+    </h1>
+);
 
 // React.createElement => Object => HTML Element (render inside a dom it become html)
 const heading = React.createElement("h1", {}, "namaste");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxElement);
+root.render(<HeaderComponent/>);
