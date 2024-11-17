fe8db2356ceb5c90cd7b636db90c0c601060ba5f
anjalichourasia <anjalichourasia1998@gmail.com>
2024-11-17 15:24:41 +0530

    useState implemented to filter the list when state change component rerender

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,27 @@
 import RestrauntCard from "./RestrauntCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    const [restList, setRestList] = useState(resObj);
+
     return(
         <div className="body">
             <div className="filter">
-                <button className="filter-btn" onClick={()=>{}}>Top rated restraunts</button>
+                <button className="filter-btn" onClick={()=>{
+                    const filteredList = restList.filter(li => li.card?.card?.info?.avgRating>4.1);
+                    setRestList(filteredList)
+                }}>Top rated restraunts</button>
             </div>
             <div className="restraunt-container">
                 {
-                    resObj.map(restraunt => <RestrauntCard 
+                    restList.map(restraunt => <RestrauntCard 
                         key = {restraunt.card?.card?.info?.id}
                         resData = {restraunt}
                     />)
                 }
                 <RestrauntCard 
-                    resData = {resObj[0]}
+                    resData = {restList[0]}
                 />
             </div>
         </div>
